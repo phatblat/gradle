539c3b5ed145e310f142b6aef0299f5a3b64b168
Adam Murdoch <adam@gradle.com>
2017-06-20 11:44:28 +1000

    Added some test coverage for validation of task input and output file properties.

--- subprojects/core/src/integTest/groovy/org/gradle/api/file/TaskFilePropertiesIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/file/TaskFilePropertiesIntegrationTest.groovy
@@ -17,6 +17,7 @@
 package org.gradle.api.file
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import spock.lang.Unroll
 
 class TaskFilePropertiesIntegrationTest extends AbstractIntegrationSpec {
     def "creates task output file and directory locations specified using annotated properties"() {
@@ -125,6 +126,10 @@ class TaskFilePropertiesIntegrationTest extends AbstractIntegrationSpec {
                 outputFilesMap = [a: file("build/files4/file4.txt"), b: file("build/files5/file5.txt")]
                 outputDirs = [file("build/dir2"), file("build/dir3")]
                 outputDirsMap = [a: file("build/dir4"), b: file("build/dir5")]
+                outputs.file("build/files6/file6.txt")
+                outputs.files("build/files7/file6.txt")
+                outputs.dir("build/dir6")
+                outputs.dirs("build/dir7")
             }
 """
 
@@ -133,6 +138,227 @@ class TaskFilePropertiesIntegrationTest extends AbstractIntegrationSpec {
 
         then:
         !file("build").exists()
+    }
+
+    def "fails when no value specified for annotated property"() {
+        buildFile << """
+            class TransformTask extends DefaultTask {
+                @${annotation}
+                File someProp
+                
+                @TaskAction
+                def go() {
+                }
+            }
+            
+            task someTask(type: TransformTask) {
+            }
+"""
+
+        when:
+        fails("someTask")
+
+        then:
+        failure.assertHasDescription("A problem was found with the configuration of task ':someTask'.")
+        failure.assertHasCause("No value has been specified for property 'someProp'.")
+
+        where:
+        annotation          | _
+        "InputFile"         | _
+        "InputFiles"        | _
+        "InputDirectory"    | _
+        "OutputFile"        | _
+        "OutputFiles"       | _
+        "OutputDirectory"   | _
+        "OutputDirectories" | _
+    }
+
+    def "fails when input location does not exist"() {
+        buildFile << """
+            class TransformTask extends DefaultTask {
+                @InputFile
+                File inputFile
+                @InputDirectory
+                File inputDir
+                
+                @TaskAction
+                def go() {
+                }
+            }
+            
+            task someTask(type: TransformTask) {
+                inputDir = file("dir1")
+                inputFile = file("file1.txt")
+            }
+"""
+
+        when:
+        fails("someTask")
+
+        then:
+        failure.assertHasDescription("Some problems were found with the configuration of task ':someTask'.")
+        failure.assertHasCause("Directory '${file("dir1")}' specified for property 'inputDir' does not exist.")
+        failure.assertHasCause("File '${file("file1.txt")}' specified for property 'inputFile' does not exist.")
+    }
+
+    def "fails when input location has incorrect type"() {
+        buildFile << """
+            class TransformTask extends DefaultTask {
+                @InputFile
+                File inputFile
+                @InputDirectory
+                File inputDir
+                
+                @TaskAction
+                def go() {
+                }
+            }
+            
+            task someTask(type: TransformTask) {
+                inputDir = file("dir1")
+                inputFile = file("file1.txt")
+            }
+"""
+
+        when:
+        file("dir1").text = "123"
+        file("file1.txt").createDir()
+
+        fails("someTask")
+
+        then:
+        failure.assertHasDescription("Some problems were found with the configuration of task ':someTask'.")
+        failure.assertHasCause("Directory '${file("dir1")}' specified for property 'inputDir' is not a directory.")
+        failure.assertHasCause("File '${file("file1.txt")}' specified for property 'inputFile' is not a file.")
+    }
+
+    def "does not validate input files declared using @InputFiles"() {
+        buildFile << """
+            class TransformTask extends DefaultTask {
+                @InputFiles
+                List<File> inputFiles
+                
+                @TaskAction
+                def go() {
+                }
+            }
+            
+            task someTask(type: TransformTask) {
+                inputFiles = [file("missing"), file("file1.txt")]
+            }
+"""
+
+        expect:
+        file("file1.txt").createDir()
+
+        succeeds("someTask")
+    }
+
+    def "does not validate input locations for ad hoc properties"() {
+        buildFile << """
+            task someTask {
+                inputs.file("missing")
+                inputs.file("file1.txt")
+                inputs.dir("missing")
+                inputs.dir("dir1")
+                inputs.files("missing", "dir1", "file1.txt")
+                doLast { }
+            }
+"""
+
+        file("dir1").text = "123"
+        file("file1.txt").createDir()
+
+        expect:
+        succeeds("someTask")
+    }
+
+    @Unroll
+    def "complains when output directory for ad hoc property cannot be created"() {
+        buildFile << """
+            task someTask {
+                outputs.${expression}
+                doLast { }
+            }
+"""
+
+        file("dir1").text = "123"
+
+        expect:
+        fails("someTask")
+        failure.assertHasDescription("Cannot create directory '${file('dir1')}' as it already exists, but is not a directory")
+
+        where:
+        expression                   | _
+        "dir('dir1')"                | _
+        "dirs('dir1', 'dir2')"       | _
+        "dirs(a: 'dir1', b: 'dir2')" | _
+    }
+
+    def "complains when output file parent directory for ad hoc property already exists and is a file"() {
+        buildFile << """
+            task someTask {
+                outputs.${expression}
+                doLast { }
+            }
+"""
+
+        file("dir1").text = "123"
+
+        expect:
+        fails("someTask")
+        failure.assertHasDescription("Cannot create directory '${file('dir1')}' as it already exists, but is not a directory")
+
+        where:
+        expression                            | _
+        "file('dir1/child')"                  | _
+        "files('dir1/child', 'child2')"       | _
+        "files(a: 'dir1/child', b: 'child2')" | _
+    }
+
+    def "fails when output location has incorrect type"() {
+        buildFile << """
+            class TransformTask extends DefaultTask {
+                @OutputFile
+                File outputFile
+                @OutputDirectory
+                File outputDir
+                @OutputFiles
+                List<File> outputFiles
+                @OutputFiles
+                Map<String, File> outputFilesMap
+                @OutputDirectories
+                List<File> outputDirs
+                @OutputDirectories
+                Map<String, File> outputDirsMap
+                
+                @TaskAction
+                def go() {
+                }
+            }
+            
+            task someTask(type: TransformTask) {
+                outputFile = file("file1.txt")
+                outputDir = file("dir1")
+                outputFiles = [file("file1.txt")]
+                outputDirs = [file("dir1")]
+                outputFilesMap = [a: file("file1.txt")]
+                outputDirsMap = [a: file("dir1")]
+            }
+"""
+
+        when:
+        file("dir1").text = "123"
+        file("file1.txt").createDir()
+
+        fails("someTask")
+
+        then:
+        failure.assertHasDescription("Some problems were found with the configuration of task ':someTask'.")
+        failure.assertHasCause("Directory '${file("dir1")}' specified for property 'outputDir' is not a directory.")
+        failure.assertHasCause("Directory '${file("dir1")}' specified for property 'outputDirs' is not a directory.")
+        failure.assertHasCause("Cannot write to file '${file("file1.txt")}' specified for property 'outputFile' as it is a directory.")
+        failure.assertHasCause("Cannot write to file '${file("file1.txt")}' specified for property 'outputFiles' as it is a directory.")
     }
 
     def "task can use Path to represent input and output locations on annotated properties"() {
@@ -201,7 +427,7 @@ class TaskFilePropertiesIntegrationTest extends AbstractIntegrationSpec {
         result.assertTasksNotSkipped(":transform")
     }
 
-    def "task can use Path to represent input and output locations on ad hoc properties"() {
+    def "task can use Path to represent input and output locations for ad hoc properties"() {
         buildFile << """
             import java.nio.file.Path
             import java.nio.file.Files
